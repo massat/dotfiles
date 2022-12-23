d9ebbe7723fbdb72701b049b93ae31fe3ef30524
massat <masato.hirai@gmail.com>
2022-12-23 17:18:56 +0900

    .で始まるエントリのシンボリックリンクを作る

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+cd `dirname $0`
+
+# ディレクトリ内の`.`で始まるエントリへのシンボリックリンクを作る
+find . -name ".*" -printf '%f\n' | grep -ve "^\.$" | grep -ve "^\.git$" \
+| xargs -I{} ln -s $(pwd)/{} $HOME/{}
 
 echo 'alias g="git"' >> ~/.bashrc

6b6af2358a54fa94a4352101c3625d232df65092
massat <masato.hirai@gmail.com>
2022-12-23 22:45:56 +0900

    peco

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+sudo usermod -s $(which zsh) $USER
+
 cd `dirname $0`
 
 # ディレクトリ内の`.`で始まるエントリへのシンボリックリンクを作る
 find . -name ".*" -printf '%f\n' | grep -ve "^\.$" | grep -ve "^\.git$" \
 | xargs -I{} ln -s $(pwd)/{} $HOME/{}
 
-echo 'alias g="git"' >> ~/.bashrc
+# load extra configs
+cat >> ~/.zshrc << TEXT
+for file in \`ls ~/.dotfiles.d/*.conf\`; do
+    source $file
+done
+TEXT
